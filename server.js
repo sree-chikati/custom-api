fa86bb67fc250866b8fd8325442ac7f0692739f9
sree-chikati <srechi453.fusdk12@gmail.com>
2021-05-03 22:02:57 -0700

    base templates and styling

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 // App Setup
 const app = express();
 
-// Allows us to use content from Public file
-app.use(express.static('public'));
+// Allows us to use content from public file
+app.use('*/css',express.static('public/css'));
 
 // Middleware
 const exphbs  = require('express-handlebars');
@@ -13,9 +13,9 @@ app.set('view engine', 'handlebars');
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('home');
 })
 
 app.listen(3000, () => {
-    console.log('Custom API listening on port localhost:3000!');
+    console.log('Gif Search listening on port localhost:3000!');
 });
